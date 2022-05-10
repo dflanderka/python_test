ec8490d5ba7ec558ca96a1aafbc68b17554a9dd7
David Flanderka <t426.001@centrum.cz>
2022-05-10 17:06:58 +0200

    Finish ExampleBase class.

--- src/example.hh
+++ src/example.hh
@@ -28,13 +28,31 @@ namespace py = pybind11;
 class ExampleBase
 {
 protected:
+    py::array create_array_with_data(double *data, ssize_t n_rows, ssize_t n_cols, ssize_t size)
+    {
+        ssize_t              ndim    = 2;
+        std::vector<ssize_t> shape   = { n_rows*n_cols , size };
+        std::vector<ssize_t> strides = { sizeof(double)*size , sizeof(double) };
+
+        // create 2-D NumPy array
+        return  py::array(py::buffer_info(
+            data,                                    /* data as contiguous array  */
+            sizeof(double),                          /* size of one scalar        */
+            py::format_descriptor<double>::format(), /* data type                 */
+            ndim,                                    /* number of dimensions      */
+            shape,                                   /* shape of the matrix       */
+            strides                                  /* strides for each axis     */
+          ));
+    }
+
     py::array create_array(ssize_t n_rows, ssize_t n_cols, ssize_t size)
     {
-        std::vector<double> result(size*n_rows*n_cols, 0.0);
+        std::vector<double> result(size*n_rows*n_cols);
+        for (uint i=0; i<result.size(); ++i) result[i] = 1.0 + i;
 
         ssize_t              ndim    = 2;
         std::vector<ssize_t> shape   = { n_rows*n_cols , size };
-        std::vector<ssize_t> strides = { sizeof(double)*n_rows*n_cols , sizeof(double) };
+        std::vector<ssize_t> strides = { sizeof(double)*size , sizeof(double) };
 
         // create 2-D NumPy array
         return  py::array(py::buffer_info(
@@ -48,7 +66,7 @@ protected:
     }
 public:
 	ExampleBase()
-	{};
+	{}
 
 	void set_time(double t)
     {
@@ -61,9 +79,19 @@ public:
     	// set self.t in Python
     }
 
+    void set_result_data(double *data, ssize_t n_rows, ssize_t n_cols, ssize_t size)
+    {
+        field_result_ = this->create_array_with_data(data, n_rows, n_cols, size);
+    }
+
     void set_result(ssize_t n_rows, ssize_t n_cols, ssize_t size)
     {
         field_result_ = this->create_array(n_rows, n_cols, size);
+    }
+
+    void add_to_dict_data(std::string field_name, double *data, ssize_t n_rows, ssize_t n_cols, ssize_t size)
+    {
+        fields_dict_[field_name.c_str()] = this->create_array_with_data(data, n_rows, n_cols, size);
     }
 
     void add_to_dict(std::string field_name, ssize_t n_rows, ssize_t n_cols, ssize_t size)
@@ -73,12 +101,12 @@ public:
 
     void print_fields() const
     {
-        std::cout << "Dictionary contains fields: ";
+        std::cout << "Dictionary contains fields: " << std::endl;
         for (auto item : fields_dict_)
         {
-        	std::cout << item.first << ", ";
+        	std::cout << " - " << item.first << ":" << std::endl << item.second << std::endl;
+        	// check item.second
         }
-        std::cout << std::endl;
     }
 
 protected:
--- test/example.cpp
+++ test/example.cpp
@@ -4,6 +4,7 @@
 
 
 #include <pybind11/pybind11.h>
+#include <pybind11/embed.h> // everything needed for embedding
 #include "example.hh"
 
 int add(int i, int j) {
@@ -19,15 +20,36 @@ PYBIND11_MODULE(myexample, m) {
         //.def(py::init<std::string, std::vector<ssize_t>, std::vector<double>>())
         .def("set_result", &ExampleBase::set_result)
 		.def("add_to_dict", &ExampleBase::add_to_dict)
+        .def("set_result_data", &ExampleBase::set_result_data)
+		.def("add_to_dict_data", &ExampleBase::add_to_dict_data)
 		.def("print_fields", &ExampleBase::print_fields)
         .def_property("t", &ExampleBase::get_time, &ExampleBase::set_time);
 
 }
 
-//int main() {
-//	int a = 1;
-//	int b = 2;
-//	int c = add(a, b);
-//	std::cout << "Add function: " << a << " + " << b << " = " << c << std::endl;
-//	return 0;
-//}
+void fill_vec(std::vector<double> &vec) {
+	for (uint i=0; i<vec.size(); ++i) vec[i] = 1.0 + i;
+}
+
+int main() {
+	py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+
+	int a = 1;
+	int b = 2;
+	int c = add(a, b);
+	std::cout << "Add function: " << a << " + " << b << " = " << c << std::endl;
+
+	std::vector<double> csection_vec(16);
+	fill_vec(csection_vec);
+	std::vector<double> velocity_vec(48);
+	fill_vec(velocity_vec);
+	std::vector<double> result_vec(48);
+	fill_vec(result_vec);
+	ExampleBase eb;
+	eb.add_to_dict_data( "csection", &(csection_vec[0]), 1, 1, 16 );
+	eb.add_to_dict_data( "velocity", &(velocity_vec[0]), 1, 3, 16 );
+	eb.set_result_data( &(result_vec[0]), 1, 3, 16 );
+	eb.print_fields();
+
+	return 0;
+}
